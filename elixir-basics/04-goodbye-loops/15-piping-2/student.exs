37274be80df52dd7008b8428c8cae4fb85ed3ec1
Jurgen Mustafa <jurgen.mustafa@student.ucll.be>
2022-03-30 14:29:25 +0200

    Piping WIP

--- elixir-basics/04-goodbye-loops/15-piping-2/student.exs
+++ elixir-basics/04-goodbye-loops/15-piping-2/student.exs
@@ -1,6 +1,10 @@
 defmodule Grades do
   def ranking(grades) do
     grades
-    |>
+    |> Enum.filter(fn {_,_,x} -> x >= 10 end)
+    |> Enum.sort_by(fn {_,_,x} -> x end, :desc)
+    |> Enum.with_index(1)
+    |> Enum.map(fn {{_,x,_},i} -> "#{i}. #{x}" end)
+    |> Enum.join("\n")
   end
 end
---
+++ elixir-basics/04-goodbye-loops/16-piping-3/student.exs
@@ -0,0 +1,8 @@
+defmodule Bank do
+  def largest_expense_index(balance_history) do
+    balance_history
+    |> Enum.zip(Enum.drop(balance_history,1))
+    |> Enum.map(fn {x,y} -> y-x end)
+    |> Enum.with_index()
+  end
+end
