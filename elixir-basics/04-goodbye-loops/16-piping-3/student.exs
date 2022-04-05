75d4fc435f7ed00cc78821957482a58c57d577f5
Jurgen Mustafa <jurgen.mustafa@student.ucll.be>
2022-04-05 20:15:37 +0200

    goodbye loops done

--- elixir-basics/04-goodbye-loops/16-piping-3/student.exs
+++ elixir-basics/04-goodbye-loops/16-piping-3/student.exs
@@ -1,8 +1,11 @@
 defmodule Bank do
   def largest_expense_index(balance_history) do
     balance_history
-    |> Enum.zip(Enum.drop(balance_history,1))
-    |> Enum.map(fn {x,y} -> y-x end)
+    |> Enum.zip(Enum.drop(balance_history, 1))
+    |> Enum.map(fn {x, y} -> y - x end)
     |> Enum.with_index()
+    |> Enum.sort_by(fn {x, _} -> x end)
+    |> Enum.map(fn {_, x} -> x end)
+    |> Enum.at(0)
   end
 end
